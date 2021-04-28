6498392e437cee0ddedad9add61fba716cf99f8d
helter daniel <helterdaniel@hotmail.com>
2021-04-28 15:34:16 -0300

    classe customer

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Customer
+
 # Register your models here.
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    """Classe para acessar os cliente no painel de administração
+
+    Arguments:
+        admin {[type]} -- [description]
+    """
+
+    pass
--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    """Classe dos clientes no banco de dados"""
+
+    # Campo nome de cada cliente
+    name = models.CharField(max_length=120)
+    # imagem de cada cliente upload_to=pasta customers contendo as imagens, default=se o cliente nao colocar imagem, sera usado uma imagem padrão
+    logo = models.ImageField(upload_to="customers", default="no_picture.png")
+
+    def __str__(self) -> str:
+        return str(self.name)
