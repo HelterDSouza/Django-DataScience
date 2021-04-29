b4be340c96b78643c7082da2d2771b3d2a5fba1d
helter daniel <helterdaniel@hotmail.com>
2021-04-28 22:10:47 -0300

    classe product e anotações

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    # nome do Produto
+    name = models.CharField(max_length=120)
+    # imagem do produto
+    image = models.ImageField(upload_to="products", default="no_picture.png")
+    # preço
+    price = models.FloatField(help_text="Em real R$")
+    # auto_now_add salva o atual timestamp quanto o objeto é criado no banco de dados
+    created = models.DateTimeField(auto_now_add=True)
+    # auto_now atualiza o atual timestamp toda vez que o objeto e salvo (last modified)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.created.strftime('%d/%m/%Y')}"
