7e913a9cbca49880a9754b0317f2a89e458d62e5
helter daniel <helterdaniel@hotmail.com>
2021-07-06 16:25:56 -0300

    Profiles signals

--- profiles/apps.py
+++ profiles/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class ProfilesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'profiles'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "profiles"
+
+    def ready(self) -> None:
+        import profiles.signals
+
+        return super().ready()
