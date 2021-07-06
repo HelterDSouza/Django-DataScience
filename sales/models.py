c4c30708595d970c8a1c840520adb5c40e81fa09
helter daniel <helterdaniel@hotmail.com>
2021-07-06 16:31:53 -0300

    classe Position

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Position(models.Model):
+    """Classe responsavel pelo valor * quantidade"""
+
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.FloatField(blank=True)
+    created = models.DateTimeField(blank=True)
+
+    def save(self, *args, **kwargs):
+        self.price = self.product.price * self.quantity
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+
+        return f"id: {self.id}, product: {self.product.name}, quantity: {self.quantity}"
+
+
+class Sale(models.Model):
+    """Responsavel pelas vendas"""
+
+    pass
