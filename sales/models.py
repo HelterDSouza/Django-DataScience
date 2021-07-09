7fed4d9b16a6a765f1f66ca70a884c2926b201e1
helter daniel <helterdaniel@hotmail.com>
2021-07-09 20:45:22 -0300

    sales model

--- sales/models.py
+++ sales/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import translation
 
 
 # Create your models here.
@@ -22,4 +23,13 @@ class Position(models.Model):
 class Sale(models.Model):
     """Responsavel pelas vendas"""
 
-    pass
+    transaction_id = models.CharField(max_length=12, blank=True)
+    positions = models.ManyToManyField(Position)
+    total_price = models.FloatField(blank=True)
+    customer = models.ForeignKey("customers.Customer", on_delete=models.CASCADE)
+    salesman = models.ForeignKey("profiles.Profile", on_delete=models.CASCADE)
+    created = models.DateTimeField(blank=True)
+    update = models.DateTimeField(auto_now=true)
+
+    def __str__(self):
+        return f"Sales of the amount of ${self.total_price}"
