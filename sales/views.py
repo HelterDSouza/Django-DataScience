95c0f7f24a3daec09cbd43e2962f567771a26da4
helter daniel <helterdaniel@hotmail.com>
2021-07-11 17:43:24 -0300

    display dataframes in the templates

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
@@ -6,11 +7,58 @@ from .form import SalesSearchForm
 
 # Create your views here.
 from .models import Sale
+from .utils import get_customer_from_id, get_salesman_from_id
 
 
 def home_view(request):
+    sales_df = None
     form = SalesSearchForm(request.POST or None)
-    context = {"form": form}
+    if request.method == "POST":
+        date_from = request.POST.get("date_from")
+        date_to = request.POST.get("date_to")
+        chart_type = request.POST.get("chart_type")
+        sales_qs = Sale.objects.filter(
+            created__date__lte=date_to, created__date__gte=date_from
+        )
+        if len(sales_qs) > 0:
+            sales_df = pd.DataFrame(sales_qs.values())
+            sales_df["customer_id"] = sales_df["customer_id"].apply(
+                get_customer_from_id
+            )
+            sales_df["salesman_id"] = sales_df["salesman_id"].apply(
+                get_salesman_from_id
+            )
+            sales_df["created"] = sales_df["created"].apply(
+                lambda x: x.strftime("%Y-%m-%d")
+            )
+            sales_df.rename(
+                {"customer_id": "customer", "salesman_id": "salesman"},
+                axis=1,
+                inplace=True,
+            )
+            positions_data = []
+            for sale in sales_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        "position_id": pos.id,
+                        "product": pos.product.name,
+                        "quantity": pos.quantity,
+                        "price": pos.price,
+                        "sales_id": pos.get_sales_id(),
+                    }
+                    positions_data.append(obj)
+
+            positions_df = pd.DataFrame(positions_data)
+
+            positions_df = positions_df.to_html()
+            sales_df = sales_df.to_html()
+        else:
+            pass
+    context = {
+        "form": form,
+        "sales_df": sales_df,
+        "positions_df": positions_df,
+    }
     return render(request, "sales/home.html", context)
 
 
